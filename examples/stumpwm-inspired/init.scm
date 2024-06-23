f0b797bc729c4c929122075de6bfce7dbb014729
Almarhoon Ibraheem <ibeem@outlook.com>
2024-06-23 20:50:39 +0300

    restructure files

--- examples/stumpwm-inspired/init.scm
+++ examples/stumpwm-inspired/init.scm
@@ -1,9 +1,22 @@
 #!/usr/bin/guile
 !#
 
-(add-to-load-path
- (dirname (or (current-filename)
-              (string-append (getenv "HOME") "/.config/sway/init.scm"))))
+;; assuming your are running from a path relative to swaypic & modules
+;; you can hardcode the load path here if that assumption isn't valid.
+;; you have to add to load path the directory the contains modules and swayipc
+;; these 2 directories exist in the root directory of the repostiry and are
+;; supposed to be 2 parent levels away from this init file.
+(let ((path (dirname
+             (dirname
+              (dirname (current-filename))))))
+  (format #t "adding folder to load path ~a\n" path)
+  (add-to-load-path path))
+
+;; you can simply uncomment the above section and hardcode the path as below
+;; (add-to-load-path "/home/YOUR_USER_HERE/git/swayipc")
+
+;; if you would like to be relative to home, do as below 
+;; (string-append (getenv "HOME") "/.config/sway/init.scm")
 
 (use-modules (oop goops)
              (srfi srfi-18)
